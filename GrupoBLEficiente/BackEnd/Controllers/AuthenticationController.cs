bff1eddf8c0f780429c4f0385c778da74f494082
Dennis Serrano <dserrano10115@ufide.ac.cr>
2024-02-28 15:45:39 -0600

    Implementando creación de usuario (Sin roles)

---
+++ GrupoBLEficiente/BackEnd/Controllers/AuthenticationController.cs
@@ -0,0 +1,211 @@
+﻿using BackEnd.Areas.Identity.Data;
+using BackEnd.Models;
+using BackEnd.Models.Authentication.SignUp;
+// using BackEnd.Service.Models;
+// using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+// using NETCore.MailKit.Core;
+// using BackEnd.Service.Services;
+using BackEnd.Models.Authentication.Login;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<GrupoBLUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        // private readonly Service.Services.IEmailService _emailService;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<GrupoBLUser> userManager, RoleManager<IdentityRole> roleManager,
+            /* Service.Services.IEmailService emailService,*/ IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            //_emailService = emailService;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("RegisterNoRoles")]
+        public async Task<IActionResult> RegisterNoRoles([FromBody] RegisterUser registerUser)
+        {
+            #region Check User Exist
+            var userExists = await _userManager.FindByNameAsync(registerUser.Username);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+            #endregion
+
+            GrupoBLUser user = new ()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+            }
+            // await _userManager.AddToRoleAsync(user, "Socio");
+
+            /*
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token, email = user.Email }, Request.Scheme);
+            var message = new Message(new string[] { user.Email! }, "Confirmation email link", confirmationLink!);
+            _emailService.SendEmail(message);
+            */
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            #region Check User Exist
+            var userExit = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExit != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", Message = "User Already Exists!" });
+            }
+            #endregion
+
+            #region Add User in DataBase
+            GrupoBLUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username,
+            };
+
+            if (await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    return (IActionResult)StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "User Failed to Created!" });
+                }
+
+                #region Assign a Role to User
+                await _userManager.AddToRoleAsync(user, role);
+                #endregion
+
+                /*
+                #region Add Token to Verify Email
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token, email = user.Email }, Request.Scheme);
+                var message = new Message(new string[] { user.Email! }, "Confirmation email link", confirmationLink!);
+                _emailService.SendEmail(message);
+                #endregion
+                */
+
+                return (IActionResult)StatusCode(StatusCodes.Status200OK,
+                    new Response { Status = "Success", Message = $"User Created Successfully & Confirmation Email Sent to: {user.Email}" });
+            }
+            else
+            {
+                return (IActionResult)StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response { Status = "Error", Message = "The Provided Role Doesn't Exist!" });
+            }
+            #endregion
+        }
+
+        /* [HttpGet]
+        [Route("TestEmail")]
+        public IActionResult TestEmail()
+        {
+            var message = new Message(new string[]
+                { "serranodennis16@gmail.com" }, "Test", "Test");
+            _emailService.SendEmail(message);
+            return StatusCode(StatusCodes.Status200OK,
+                new Response { Status = "Success", Message = "Email Sent Successfully!" });
+        } */
+
+        [HttpGet("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status200OK,
+                      new Response { Status = "Success", Message = "Email Verified Successfully!" });
+                }
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                       new Response { Status = "Error", Message = "This User Doesn't Exist!" });
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginmodel)
+        {
+            var user = await _userManager.FindByNameAsync(loginmodel.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginmodel.Password))
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                var userRoles = await _userManager.GetRolesAsync(user);
+                foreach (var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var jwtToken = GetToken(authClaims);
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                    expiration = jwtToken.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(2),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return token;
+        }
+
+        [HttpPost]
+        [Route("GetUser")]
+        public async Task<IActionResult> GetUser([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+
+                model.Roles = userRoles.ToList();
+
+
+                return Ok(model);
+            }
+            return Unauthorized();
+        }
+    }
+}
