3b8c731a2b563c428b2ad46919f9e6e9fee8af24
Dennis Serrano <dserrano10115@ufide.ac.cr>
2024-03-04 17:36:22 -0600

    Implementando FE del CRUD de NationalIdType

---
+++ GrupoBLEficiente/FrontEnd/Controllers/NationalIdTypeController.cs
@@ -0,0 +1,110 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class NationalIdTypeController : Controller
+    {
+        NationalIdTypeHelper entityHelper;
+
+        #region Index
+        // GET: Controller
+        public ActionResult Index()
+        {
+            entityHelper = new NationalIdTypeHelper();
+            List<NationalIdTypeViewModel> list = entityHelper.GetAll();
+            return View(list);
+        }
+        #endregion
+
+        #region Details
+        // GET: Controller/Details/5
+        public ActionResult Details(int id)
+        {
+            entityHelper = new NationalIdTypeHelper();
+            NationalIdTypeViewModel entity = entityHelper.GetByID(id);
+            return View(entity);
+        }
+        #endregion
+
+        #region Create
+        // GET: Controller/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Controller/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NationalIdTypeViewModel entity)
+        {
+            try
+            {
+                entityHelper = new NationalIdTypeHelper();
+                entity = entityHelper.Add(entity);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Edit
+        // GET: Controller/Edit/5
+        public ActionResult Edit(int id)
+        {
+            entityHelper = new NationalIdTypeHelper();
+            NationalIdTypeViewModel entity = entityHelper.GetByID(id);
+            return View(entity);
+        }
+
+        // POST: Controller/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(NationalIdTypeViewModel entity)
+        {
+            try
+            {
+                entityHelper = new NationalIdTypeHelper();
+                entity = entityHelper.Edit(entity);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Delete
+        // GET: Controller/Delete/5
+        public ActionResult Delete(int id)
+        {
+            entityHelper = new NationalIdTypeHelper();
+            NationalIdTypeViewModel entity = entityHelper.GetByID(id);
+            return View(entity);
+        }
+
+        // POST: Controller/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(NationalIdTypeViewModel entity)
+        {
+            try
+            {
+                entityHelper = new NationalIdTypeHelper();
+                entityHelper.Delete(entity.IdType);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+    }
+}
---
+++ GrupoBLEficiente/FrontEnd/Helpers/NationalIdTypeHelper.cs
@@ -0,0 +1,69 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class NationalIdTypeHelper
+    {
+        ServiceRepository repository;
+
+        public NationalIdTypeHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        #region GetAll
+        public List<NationalIdTypeViewModel> GetAll()
+        {
+            List<NationalIdTypeViewModel> entities = new List<NationalIdTypeViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/NationalIdType/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                entities = JsonConvert.DeserializeObject<List<NationalIdTypeViewModel>>(content);
+            }
+            return entities;
+        }
+        #endregion
+
+        #region GetByID
+        public NationalIdTypeViewModel GetByID(int id)
+        {
+            NationalIdTypeViewModel entity = new NationalIdTypeViewModel();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/NationalIdType/" + id);
+            string content = responseMessage.Content.ReadAsStringAsync().Result;
+            entity = JsonConvert.DeserializeObject<NationalIdTypeViewModel>(content);
+            return entity;
+        }
+        #endregion
+
+        #region Update
+        public NationalIdTypeViewModel Edit(NationalIdTypeViewModel entity)
+        {
+            HttpResponseMessage responseMessage = repository.PutResponse("api/NationalIdType/", entity);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            NationalIdTypeViewModel entityAPI = JsonConvert.DeserializeObject<NationalIdTypeViewModel>(content);
+            return entityAPI;
+        }
+        #endregion
+
+        #region Create
+        public NationalIdTypeViewModel Add(NationalIdTypeViewModel entity)
+        {
+            HttpResponseMessage responseMessage = repository.PostResponse("api/NationalIdType/", entity);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            NationalIdTypeViewModel entityAPI = JsonConvert.DeserializeObject<NationalIdTypeViewModel>(content);
+            return entityAPI;
+        }
+        #endregion
+
+        #region Delete
+        public NationalIdTypeViewModel Delete(int id)
+        {
+            NationalIdTypeViewModel entity = new NationalIdTypeViewModel();
+            HttpResponseMessage responseMessage = repository.DeleteResponse("api/NationalIdType/" + id);
+            return entity;
+        }
+        #endregion
+    }
+}
